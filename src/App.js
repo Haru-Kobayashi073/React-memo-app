ddf3f9ca7ba64ad8d40d4460bbacd10da864be16
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2022-11-07 09:48:16 +0900

    初期UI

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+import { useFetchMemo } from './hooks';
+import { 
+  Header,
+  Body,
+  Modal,
+  Detail,
+  Loading,
+  List
+} from './components';
+
+function App() {
+  const [open, setOpen] = useState(false)
+  const {memos, loading} = useFetchMemo()
+  const [memo, setMemo] = useState({})
+
+  const closeHandler = () => setOpen(false)
+
+  const clickHandler = (id) => {
+    setMemo(memos.find(memo => memo.id === id))
+    setOpen(true)
+  }
+
+  return (
+    <>
+      <Header/>
+      <Body>
+        <h2 style={{padding: '10px 0 20px'}}>メモ一覧</h2>
+        {loading && <Loading/>}
+        <List data={memos} clickHandler={clickHandler}/>
+      </Body>
+      <Modal open={open} closeHandler={closeHandler}>
+        <Detail memo={memo}/>
+      </Modal>
+    </>
+  );
+}
+
+export default App;
